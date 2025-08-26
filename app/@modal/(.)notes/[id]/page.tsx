a3e8b304d1391192ea4c792a03a71560e16ad93b
Skaleivt <skaleiviktor@gmail.com>
2025-08-26 12:06:13 +0300

    12

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { fetchNoteById } from "@/lib/api";
+import { fetchNoteById } from "@/lib/api/serverApi";
 import NotePreviewModal from "./NotePreview.client";
 import { QueryClient, dehydrate } from "@tanstack/react-query";
 
